be2361e4498d456fcdbbcd1877383e2230140c1c
Thauã Borges <thaua97@gmail.com>
2025-09-16 18:04:26 -0300

    feat: altera rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Route } from '@angular/router';
 import { FormRegistrationShellComponent } from './features/form-registration/pages/form/form.component';
 import { ResumeComponent } from './features/resume/pages/resume/resume.component';
+import { Resumes } from './features/resume/pages/resumes/resumes';
 
 export const routes: Route[] = [
     {
         path: '',
         component: FormRegistrationShellComponent,
     },
-    {   
+    {
         path: 'resumes',
-        component: ResumeComponent
-    }
+        component: Resumes,
+    },
+		{
+			path: 'resumes/:id/candidate',
+			component: ResumeComponent,
+		}
 ];
