2b40ec1c4a2be32686172a4d9e792e1d422a3f33
Thauã Borges <thaua97@gmail.com>
2025-09-16 08:10:47 -0300

    chore: remove componente desnecessario

--- src/app/features/form-registration/components/step-wrapper/step.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, Input, TemplateRef } from '@angular/core';
-
-@Directive({
-  selector: '[step]'
-})
-export class StepTemplateDirective {
-  @Input('step') step!: number;
-  constructor(public template: TemplateRef<any>) {}
-}
